d3c828de8f2fb608d8d9afb72f66365a7d939241
Prafful Daga <prraful@gmail.com>
2020-02-26 15:45:18 +0530

    axios child to parent

--- src/remote/data.js
+++ src/remote/data.js
@@ -2,11 +2,32 @@ import React from 'react';
 
 class UserData extends React.Component {
     name = "Prafful Daga"
+
+    constructor(props){
+        super(props)
+        this.userClicked = this.userClicked.bind(this)
+    }
+
+    userClicked = function(){
+        console.log("User clicked with id: " + this.props.id);
+        this.props.callme(this.props.id)
+    }
+
     render() { 
         return (
-            <div>
-               Name: {this.props.name} from {this.props.company}, {this.props.city}
-            </div>
+            <tr>
+                <td>{this.props.id}</td>
+               <td>{this.props.name}</td>
+               <td>{this.props.company}</td>  
+               <td>{this.props.city}</td>
+               <td>
+                    <button onClick={this.userClicked}>Edit</button>
+               </td>
+               <td>
+                    <button>Delete</button>
+               </td>
+             
+            </tr>
           );
     }
 }
--- src/remote/remote.js
+++ src/remote/remote.js
@@ -11,6 +11,8 @@ class RemoteData extends React.Component {
             singleuser:''
         }
         this.success = this.success.bind(this)
+        this.callMeForEachElement = this.callMeForEachElement.bind(this)
+        this.userCalledMe = this.userCalledMe.bind(this)
     }
 
 
@@ -40,13 +42,22 @@ class RemoteData extends React.Component {
         return this.state.users.map(this.callMeForEachElement)
     }
 
+    userCalledMe = function(receivedID){
+        console.log("I am being called from Child Component Via Props for: " + receivedID);
+        console.log("I am working with following JSON object from array of this.state.users: ");
+        console.log(this.state.users[receivedID-1]);
+
+    }
+
     callMeForEachElement = function(user){
         console.log(user.name);
         return (<UserData
-                    key={user.id}
+                    key = {user.id}
+                    id= {user.id}
                     name={user.name}
                     city={user.address.city}
                     company={user.company.name}
+                    callme = {this.userCalledMe}
                 ></UserData>)
     }
 
@@ -55,7 +66,22 @@ class RemoteData extends React.Component {
         return ( 
             <div>
                 Data from REST API will be loaded here!
-                {this.displayAllUsers()}
+                <table border="1">
+                    
+                
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>City</th>
+                        <th>Company</th>
+                        <th colspan="2">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.displayAllUsers()}
+                </tbody>
+                </table>
             </div>
          );
     }
